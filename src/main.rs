569df9384743ed3c8508e647fd7fcbb17b6dac45
Daniel <davilareko@gmail.com>
2024-09-08 19:03:57 +0200

    Rewrite read

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,27 @@
-use case_insensitive_hashmap::CaseInsensitiveHashMap;
-mod http_lib;
-
+use may_minihttp::{HttpServer, HttpService, Request, Response};
+use std::io;
 mod handlers;
+mod http_lib;
 mod utils_lib;
+#[derive(Clone)]
+struct RealFS;
 
-fn main() {
-    let handler = |incoming_req: http_lib::RequestDataToSet| -> http_lib::ResponseDataToSet {
-        println!("got request");
-        let mut method = "GET".to_string();
-        let mut path = "/".to_string();
-        // for http_method in http_lib::COMMON_HTTP_METHODS {
-        //     if incoming_req.method_and_path.starts_with(http_method) {
-        //         method = &incoming_req.method_and_path[..http_method.chars().count()].trim_end();
-        //         path = &incoming_req.method_and_path[http_method.chars().count()..].trim_start();
-        //         break;
-        //     }
-        // }
-        http_lib::extract_path_and_method(
-            incoming_req.method_and_path.as_str(),
-            &mut path,
-            &mut method,
-        );
-        // println!("method: {}\npath: {}", method, path);
-        // for (key, value) in &incoming_req.base.headers {
-        //     println!("header: {}: {}", key, value);
-        // }
-        let send_error = |code: i32, msg: &str| http_lib::ResponseDataToSet {
-            base: http_lib::BasicHTTPDataToSet {
-                headers: CaseInsensitiveHashMap::new(),
-                data: [].to_vec(),
-            },
-            code,
-            msg: msg.to_string(),
-        };
-        // let method_str = method.as_str();
+impl HttpService for RealFS {
+    fn call(&mut self, req: Request, res: &mut Response) -> io::Result<()> {
+        let path = req.path();
+        let method = req.method();
         if path.starts_with(handlers::read::BASE) && method == handlers::read::METHOD {
             return handlers::read::handle_read(
-                incoming_req,
+                req,
                 (std::env::args().collect::<Vec<String>>()[1]).as_str(),
+                res,
             );
         }
-        if path.starts_with(handlers::list::BASE) && method == handlers::list::METHOD {
-            return handlers::list::handle_list(
-                incoming_req,
-                (std::env::args().collect::<Vec<String>>()[1]).as_str(),
-            );
-        }
-        if path.starts_with(handlers::write::BASE) && method == handlers::write::METHOD {
-            return handlers::write::handle_write(
-                incoming_req,
-                (std::env::args().collect::<Vec<String>>()[1]).as_str(),
-            );
-        }
-        if path.starts_with(handlers::stat::BASE) && method == handlers::stat::METHOD {
-            return handlers::stat::handle_stat(
-                incoming_req,
-                (std::env::args().collect::<Vec<String>>()[1]).as_str(),
-            );
-        }
-        send_error(500, "Server error")
-    };
-    // http_lib::start_server(8080, handler);
-    http_lib::start_server(2137, handler);
+        Ok(())
+    }
+}
+
+fn main() {
+    let server = HttpServer(RealFS).start("0.0.0.0:2137").unwrap();
+    server.join().unwrap();
 }
