da688aa2dc4d762892c0806feeb679cc7eea92a3
Daniel <davilareko@gmail.com>
2024-09-14 22:03:44 +0200

    Remove comments

--- src/main.rs
+++ src/main.rs
@@ -33,36 +33,13 @@ impl HttpService for RealFS {
 fn main() {
     may::config().set_stack_size(4096 * 2);
     unsafe {
-        // let mut t: Vec<String> = Vec::new();
-        // for n in std::env::args() {
-        //     t.push(n);
-        // };
         let t = Box::into_raw(Box::new(std::env::args().collect::<Vec<String>>()));
-        // let mut t2 = Vec::new();
-        // let mut aaa = &mut String::new();
-        // for n in t {
-        //     let a: &mut String = aaa.borrow_mut();
-        //     aaa.clone_from(&mut n.clone());
-        //     t2.push(aaa.as_str());
-        // }
-        // let t2 = t
-        //     .to_owned()
-        //     .as_ref()
-        //     .unwrap()
-        //     .iter()
-        //     .map(|x| x.as_str())
-        //     .collect::<Vec<&str>>();
-        // let target = t2.get(1).unwrap();
         let target = t.as_ref().unwrap().get(1).unwrap();
         println!("{target}");
-        // let target_raw = Box::into_raw(Box::new(target));
-        // MNT = *target_raw.clone();
         let target_raw = String::from(target);
         let target_raw2 = Box::into_raw(Box::new(target_raw));
         MNT = target_raw2.as_ref().unwrap().as_str();
         t.drop_in_place();
-        // target_raw2.drop_in_place();
-        // drop(Box::from_raw(t));
     };
     let server = HttpServer(RealFS).start("0.0.0.0:2137").unwrap();
     server.join().unwrap();
